7094dddf63a268f17189ab8a0ee94c83aaca6e59
SchmitzNate <schmitznate@gmail.com>
2014-04-08 09:26:48 -0400

    working description

--- front_end.js
+++ front_end.js
@@ -1,7 +1,6 @@
 
 // http://jshint.com to ensure correctness.
 
-
 function SchoolPeriod(period) {
     
     this.block = period['block'];
@@ -11,8 +10,8 @@ function SchoolPeriod(period) {
     this.getInfo = function() {
         
         return this.block + " ----- " +
-            this.start.toTimeString() + " -> " +
-            this.end.toTimeString();
+        this.start.toTimeString() + " -> " +
+        this.end.toTimeString();
         
     };
 }
@@ -43,9 +42,9 @@ function SchoolDay(now, key, catalog) {
     this.getInfo = function() {
         
         return this.dayType + " " +
-            this.isSchool + " " +
-            this.start.toTimeString() + " " +
-            this.end.toTimeString();
+        this.isSchool + " " +
+        this.start.toTimeString() + " " +
+        this.end.toTimeString();
         
     };
     
@@ -54,13 +53,14 @@ function SchoolDay(now, key, catalog) {
         var cpi = -1;
         var i = this.periods.length;
         
-        while (i--)
-        {
+        while (i--) {
+            
             var perStart = this.periods[i].start;
             var perEnd = this.periods[i].end;
             if ((now.getTime() >= perStart.getTime() &&
-                 now.getTime() <= perEnd.getTime()))
+                 now.getTime() <= perEnd.getTime())) {
                 cpi = i;
+            }
         }
         
         return cpi;
@@ -75,12 +75,43 @@ function SchoolDay(now, key, catalog) {
         return -1;
     };
     
-    this.currentPeriodIndex = this.getCurrentPeriod();
-    this.nextPeriodIndex = this.getNextPeriod();
+    this.curPerInd = this.getCurrentPeriod();
+    this.nxtPerInd = this.getNextPeriod();
     
     this.getDescription = function() {
         
+        var desc = "";
         
+        if (this.isSchool === true) {
+            
+            if (now.getTime() < this.start) {
+                var firstPeriod = this.periods[this.nxtPerInd];
+                desc = desc + "Your first class, " + firstPeriod.block +
+                       ", starts in " + getTextTimeDifference(now, firstPeriod.start) + ".";
+            }
+            else if (now.getTime() >= this.end) {
+                desc = desc + "You have no more classes today.";
+            }
+            else if (this.curPerInd < 0) {
+                var nextPeriod = this.periods[this.nxtPerInd];
+                desc = desc + "You have " + getTextTimeDifference(now, nextPeriod.start) +
+                       " to get to " + nextPeriod.block + ".";
+            }
+            else {
+                
+                var curPer = this.periods[this.curPerInd];
+                desc = desc + "You have " + getTextTimeDifference(now, curPer.end) + " left in " + curPer.block + ".";
+                
+                if (this.nxtPerInd > 0)
+                    desc = desc + " Your next period is " + this.periods[this.nxtPerInd].block + ".";
+                
+            }
+        }
+        else {
+            desc = desc + "You have no school today (" + this.dayType + ").";
+        }
+        
+        return desc;
         
     }
     
@@ -105,27 +136,37 @@ function updateDisplay() {
     var key = now.toDateString();
     
     var sd = new SchoolDay(now, key, window.schedule);
-    
     var dtStr = now.toLocaleString() + " (" + sd.dayType + ")";
     
     datetime.innerHTML = dtStr + "<br>";
-    debug.innerHTML = sd.description + "<br><br>";
+    debug.innerHTML = sd.getDescription() + "<br><br>";
     
-    if (sd.isSchool == true)
+    if (sd.isSchool === true)
         for (var i = 0, ii = sd.periods.length; i < ii; i++)
             debug.innerHTML = debug.innerHTML + sd.periods[i].getInfo() + "<br>";
+}
+
+function getWordsFromSeconds(s) {
+    var m = Math.floor(s / 60.0);
+    var h = Math.floor(s / (60.0 * 60.0));
+    if (h > 0) return h + " hour" + ((h != 1) ? "s" : "");
+    else if (m > 0) return m + " minute" + ((m != 1) ? "s" : "");
+    else return s + " second" + ((s != 1) ? "s" : "");
+}
+
+function getTextTimeDifference(first, second) {
+    var s = Math.ceil((second.getTime() - first.getTime()) / 1000.0);
+    return getWordsFromSeconds(s);
 }
 
 function getFullSchedule() {
     
     var xmlhttp;
     
-    if (window.XMLHttpRequest)
-    {
+    if (window.XMLHttpRequest) {
         xmlhttp = new XMLHttpRequest();
     }
-    else
-    {
+    else {
         xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
     }
     
